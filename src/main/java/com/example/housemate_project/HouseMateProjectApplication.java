7f848fa0533199b815a4f9c89f648ce1fb8a578d
Kwonnamhyung <rnjsskagud@gmail.com>
2022-10-22 13:13:22 +0900

    initial commit

---
+++ src/main/java/com/example/housemate_project/HouseMateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.housemate_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseMateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseMateProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/housemate_project/HouseMateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.housemate_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseMateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
